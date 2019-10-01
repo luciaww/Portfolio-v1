340a299c3d732f30d8ca37cbc4d2703cc873e797
luciaww <theluciaw@gmail.com>
2019-10-01 01:43:18 -0400

    pre-fix

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import Project from './Project'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import h from './img/oke.jpg'
+import h2 from './img/h.png'
+import h3 from './img/h2.png'
+
+class App extends Component {
+  render() {
+    const projects = [
+      {
+        company: 'Spectrum, 2019',
+        name: 'Motion Design System',
+        desc: 'Spearheaded the motion design system at Spectrum during the course of a 10 week internship.',
+        url: h,
+      },
+      {
+        company: 'Element, 2019',
+        name: 'Cloud Gaming UI Design',
+        desc: 'Created initial mockups for a new cloud gaming platform.',
+        url: h2,
+      },
+      {
+        company: 'ARTS200, 2019',
+        name: 'Pet Adoption App Design',
+        desc: 'Worked on designing all screens for an application that would allow pet adopters to find their forever companions.',
+        url: h3,
+      },
+    ]
+
+
+    return(
+      <div className="container">
+        <Project projectData={projects} />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ portfolio/src/Project.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import './Project.css';
+
+const ProjectBody = props => {
+    const obj = props.projectData.map((row, index) => {
+        return (
+            <tr key={index} >
+                <div class="project-container">
+                    <div class="project">
+                        <div class="project-content">
+                            <div class="project-label">{row.company}</div>
+                                <h4 class="project-title">{row.name}</h4>
+                                <div class="project-details">
+                                    <p>{row.desc}</p>
+                                    <ul>
+                                        <li>Shadow</li>
+                                        <li>Demon</li>
+                                        <li>Baby</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        
+                        <div class="project-img">
+                          <img src={row.url} />
+                        </div>
+                    </div>
+                </div>
+            </tr>
+        )
+    })
+
+    return <div className="surrounding">{obj}</div>
+}
+
+class Project extends Component{
+    render(){
+        const { projectData } = this.props
+        
+        return(
+            <ProjectBody projectData={projectData} />
+        )
+    }
+}
+
+export default Project
