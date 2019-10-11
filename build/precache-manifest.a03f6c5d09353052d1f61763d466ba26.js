b929b18a9ca9676d0a8df99a59c6e886fe721b67
luciaww <theluciaw@gmail.com>
2019-10-10 21:44:16 -0400

    delt

---
+++ build/precache-manifest.a03f6c5d09353052d1f61763d466ba26.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0ae90002375813c0702630ae364288d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4405335cf19c9505964a",
+    "url": "/static/css/main.fcc56812.chunk.css"
+  },
+  {
+    "revision": "d8f06622255c39592342",
+    "url": "/static/js/2.2c67406b.chunk.js"
+  },
+  {
+    "revision": "4405335cf19c9505964a",
+    "url": "/static/js/main.17dd45fe.chunk.js"
+  },
+  {
+    "revision": "a6e3dc98bf14d00cdb8a",
+    "url": "/static/js/runtime-main.c5683173.js"
+  },
+  {
+    "revision": "45e3bacab7c57da68f3f91a31b6a2ddf",
+    "url": "/static/media/GTWalsheimProRegular.45e3baca.ttf"
+  },
+  {
+    "revision": "24abaafe1435fbbb1da0d2f2cdf41a98",
+    "url": "/static/media/h.24abaafe.png"
+  },
+  {
+    "revision": "3900fd57498ee6c88de57fc84a666b95",
+    "url": "/static/media/h2.3900fd57.png"
+  },
+  {
+    "revision": "53c4e236ddbd5b47b0932e525d7118ad",
+    "url": "/static/media/tab.53c4e236.gif"
+  }
+]);
